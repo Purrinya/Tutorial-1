14918698c20898f21599b9464e82172728a310a8
Purrinya <Boyvietha200@gmail.com>
2021-10-22 14:09:13 +0700

    Update

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+# Comment
+# Write comments or explanation here
+# Input: three number a, b, c
+# Output:   1. 3 side of a triangle or not?
+#           2. Triangle --> what kind of triangle?
+
+a = 3
+b = 4
+c = 5
 print("Hello meow")
